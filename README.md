bf8461ef3edc8e07fe81d7dd76f3fe7d89c4767d
Steve <stevechez@gmail.com>
2024-01-15 04:00:48 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Build SEO Optimized Personal Blog with Next.js, Contentlayer, and Tailwind CSS
+
+<https://www.youtube.com/watch?v=nkGjob3q2GI>
---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+    title: 'Be Fit and Thrive Blog',
+    author: 'Steve Chez',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog focused on fitness, nutrition, mindfullness, and overall health-related matters.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://be-fit-and-thrive.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.jpg', // add social banner in the public folder
+    email: 'stevechezdev@gmail.com', 
+    github: 'https://github.com/',
+    twitter: 'https://twitter.com/',
+    facebook: 'https://facebook.com',
+    youtube: 'https://youtube.com/',
+    linkedin: 'https://www.linkedin.com/',
+    dribbble: 'https://www.dribbble.com',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
